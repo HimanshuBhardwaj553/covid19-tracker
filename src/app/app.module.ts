f8c9f4c4f9ba3edaeb49a6de663297fa075ec38b
himanshubhardwaj553 <himanshubhardwaj553@gmail.com>
2020-10-29 22:09:38 +0530

    covid19-tracker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CountriesComponent } from './components/countries/countries.component';
+import {HttpClientModule} from '@angular/common/http';
+import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component'
+import { GoogleChartsModule } from 'angular-google-charts';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    DashboardCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    GoogleChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
